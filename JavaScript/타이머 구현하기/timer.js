4e8bbcce1f82f388c201710df109fc569454a61d
uussong <77879633+uussong@users.noreply.github.com>
2023-05-09 11:37:47 +0900

    ✨ runTimer 일부 구현

--- JavaScript/타이머 구현하기/timer.js
+++ JavaScript/타이머 구현하기/timer.js
@@ -5,13 +5,26 @@ const second = document.querySelector('#second')
 const btnStart = document.querySelector('.start')
 const btnReset = document.querySelector('.reset')
 
+// 시간 입력 시 버튼 활성화 - 타이머 시작 준비
 function startTimer() {
   btnStart.querySelector('img').src = './img/icon-start.png'
+  btnStart.disabled = false
   btnStart.classList.add('active')
   btnReset.querySelector('img').src = './img/icon-reset.png'
   btnReset.classList.add('active')
+  btnReset.disabled = false
 }
 
 hour.addEventListener('keypress', startTimer)
 minute.addEventListener('keypress', startTimer)
 second.addEventListener('keypress', startTimer)
+
+// 타이머 동작 
+function runTimer() {
+  // START -> PAUSE
+  btnStart.querySelector('img').src = './img/icon-stop.png'
+  btnStart.style.backgroundColor = '#15C2B8'
+  btnStart.innerHTML = `<img src='./img/icon-stop.png' alt="일시 정지"> PAUSE`
+}
+
+btnStart.addEventListener('click', runTimer)
