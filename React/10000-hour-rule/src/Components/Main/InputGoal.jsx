f4d6e762e01fea12fde6be41509460291bccf051
uussong <77879633+uussong@users.noreply.github.com>
2023-05-21 23:13:54 +0900

    🎨 onClick 아닌 form onSumit으로 데이터 전송되도록 개선

--- React/10000-hour-rule/src/Components/Main/InputGoal.jsx
+++ React/10000-hour-rule/src/Components/Main/InputGoal.jsx
@@ -4,7 +4,8 @@ import { useState } from 'react'
 
 export default function InputGoal({ setData }) {
   
-  function handleSubmit() {
+  function handleSubmit(e) {
+    e.preventDefault()
     const formData = {
       goal: goal,
       time: time
@@ -17,17 +18,19 @@ export default function InputGoal({ setData }) {
 
   return (
     <section className='inputGoal'>
-      <p className='goal'>
-        나는
-        <input type="text" placeholder='예)프로그래밍' value={goal} onChange={event => setGoal(event.target.value)} />
-        전문가가 될 것이다.
-      </p>
-      <p className='goal'>
-        그래서 앞으로 매일 하루에
-        <input type="number" placeholder='예)5시간' value={time} onChange={event => setTime(event.target.value)} />
-        시간씩 훈련할 것이다.
-      </p>
-      <button type='submit' className='btnSubmit' onClick={handleSubmit}>나는 며칠 동안 훈련을 해야 1만 시간이 될까?</button>
+      <form onSubmit={handleSubmit}>
+        <p className='goal'>
+          나는
+          <input type="text" placeholder='예)프로그래밍' value={goal} onChange={event => setGoal(event.target.value)} />
+          전문가가 될 것이다.
+        </p>
+        <p className='goal'>
+          그래서 앞으로 매일 하루에
+          <input type="number" placeholder='예)5시간' value={time} onChange={event => setTime(event.target.value)} />
+          시간씩 훈련할 것이다.
+        </p>
+        <button type='submit' className='btnSubmit'>나는 며칠 동안 훈련을 해야 1만 시간이 될까?</button>
+      </form>
     </section>
   )
 }
